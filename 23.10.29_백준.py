81d9b406525fec0c8483336b91ccd503b1aa8343
HyunJunLee-Hi <nejukja@gmail.com>
2023-10-29 21:24:16 +0900

    first commit

---
+++ 23.10.29_백준.py
@@ -0,0 +1,19 @@
+#2839 설탕 배달
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+res = 0
+
+while n >= 0:
+    if n%5 == 0:
+        res += n//5
+        print(res)
+        break
+
+    n -= 3
+    res += 1
+
+else:
+    print(-1)
+
